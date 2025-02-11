59e89e56e9210d32be3bda1ef90b2703ce60f0b8
Laure Thompson <602628+laurejt@users.noreply.github.com>
2025-02-11 12:19:15 -0500

    Add span index validation on initialization

--- src/corppa/poetry_detection/poem_excerpts.py
+++ src/corppa/poetry_detection/poem_excerpts.py
@@ -29,6 +29,17 @@ class PoemExcerpt:
     identification_methods: set[str]
     notes: Optional[str] = None
 
+    def __post_init__(self):
+        # Check PPA span indices
+        if self.ppa_span_end <= self.ppa_span_start:
+            raise ValueError("PPA span's start index must be less than end index")
+        # Check that both reference span indicies are set or unset
+        if (self.ref_span_start is None) ^ (self.ref_span_end is None):
+            raise ValueError("Reference span's start and end index must both be set")
+        # Check reference span indices if set
+        if self.ref_span_end is not None and self.ref_span_end <= self.ref_span_start:
+            raise ValueError("Reference span's start index must be less than end index")
+
     def to_dict(self) -> dict[str, Any]:
         """
         Returns a JSON-friendly dict of the poem excerpt. Note that unset optional fields
--- test/test_poetry_detection/test_poem_excerpts.py
+++ test/test_poetry_detection/test_poem_excerpts.py
@@ -4,6 +4,86 @@ from corppa.poetry_detection.poem_excerpts import PoemExcerpt
 
 
 class TestPoemExcerpt:
+    def test_init(self):
+        # Invalid PPA span indices
+        error_message = "PPA span's start index must be less than end index"
+        with pytest.raises(ValueError, match=error_message):
+            poem_excerpt = PoemExcerpt(
+                page_id="page_id",
+                ppa_span_start=0,
+                ppa_span_end=0,
+                ppa_span_text="page_text",
+                poem_id="poem_id",
+                ref_corpus="corpus_id",
+                detection_methods={"detect"},
+                identification_methods={"id"},
+            )
+        with pytest.raises(ValueError, match=error_message):
+            poem_excerpt = PoemExcerpt(
+                page_id="page_id",
+                ppa_span_start=1,
+                ppa_span_end=0,
+                ppa_span_text="page_text",
+                poem_id="poem_id",
+                ref_corpus="corpus_id",
+                detection_methods={"detect"},
+                identification_methods={"id"},
+            )
+        # Partially unset reference span indices
+        error_message = "Reference span's start and end index must both be set"
+        with pytest.raises(ValueError, match=error_message):
+            poem_excerpt = PoemExcerpt(
+                page_id="page_id",
+                ppa_span_start=0,
+                ppa_span_end=1,
+                ppa_span_text="page_text",
+                poem_id="poem_id",
+                ref_corpus="corpus_id",
+                detection_methods={"detect"},
+                identification_methods={"id"},
+                ref_span_start=0,
+            )
+        with pytest.raises(ValueError, match=error_message):
+            poem_excerpt = PoemExcerpt(
+                page_id="page_id",
+                ppa_span_start=0,
+                ppa_span_end=1,
+                ppa_span_text="page_text",
+                poem_id="poem_id",
+                ref_corpus="corpus_id",
+                detection_methods={"detect"},
+                identification_methods={"id"},
+                ref_span_end=1,
+            )
+        # Invalid reference span indices
+        error_message = "Reference span's start index must be less than end index"
+        with pytest.raises(ValueError, match=error_message):
+            poem_excerpt = PoemExcerpt(
+                page_id="page_id",
+                ppa_span_start=0,
+                ppa_span_end=1,
+                ppa_span_text="page_text",
+                poem_id="poem_id",
+                ref_corpus="corpus_id",
+                detection_methods={"detect"},
+                identification_methods={"id"},
+                ref_span_start=0,
+                ref_span_end=0,
+            )
+        with pytest.raises(ValueError, match=error_message):
+            poem_excerpt = PoemExcerpt(
+                page_id="page_id",
+                ppa_span_start=0,
+                ppa_span_end=1,
+                ppa_span_text="page_text",
+                poem_id="poem_id",
+                ref_corpus="corpus_id",
+                detection_methods={"detect"},
+                identification_methods={"id"},
+                ref_span_start=1,
+                ref_span_end=0,
+            )
+
     def test_to_dict(self):
         # No optional fields
         poem_excerpt = PoemExcerpt(
