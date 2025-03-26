2db3b8d18bf5db566c39465aaf574c55fcab5002
rlskoeser <rebecca.s.koeser@princeton.edu>
2025-03-26 14:19:38 -0400

    Handle default config values & overrides

--- src/corppa/config.py
+++ src/corppa/config.py
@@ -19,6 +19,44 @@ CORPPA_CONFIG_PATH = CORPPA_SRC_DIR.parent / "corppa_config.yml"
 #: expected path for example config file
 SAMPLE_CONFIG_PATH = CORPPA_SRC_DIR.parent / "sample_config.yml"
 
+#: default configuration; anything in yaml config will override
+DEFAULTS = {
+    "reference_corpora": {
+        # base_dir path is relative to top-level data_ingredients_dir
+        "base_dir": "ref-corpora",
+        # paths are relative to base_dir
+        "internet_poems": {
+            # tarball of directory of text files OR expanded directory
+            "text_dir": "internet_poems/internet_poems_texts.tar.gz"
+        },
+        "chadwyck-healey": {
+            "text_dir": "chadwyck-healey/chadwyck-healey_texts.tar.gz",
+            "metadata_path": "chadwyck-healey/chadwyck-healey.csv",
+        },
+        # other poems metadata_path configuration required
+    }
+}
+
+
+def merge_dicts(initial_values: dict, overrides: dict) -> dict:
+    """Recursively merge two dictionaries. Anything values in second dict
+    take precedence over the first. Nested dictionaries are merged
+    by calling :meth:`merge_dicts`.
+    """
+    merged_dict = initial_values.copy()
+    for key, value in overrides.items():
+        # if the override value is a dict and key is present in both,
+        # then merge the nested dicts
+        if isinstance(value, dict) and key in merged_dict:
+            # note: assumes they are both dicts; this should be safe
+            # for our config file setup
+            merged_dict[key] = merge_dicts(initial_values[key], overrides[key])
+        # otherwise, set the value from the override dictionary
+        else:
+            merged_dict[key] = value
+
+    return merged_dict
+
 
 def get_config():
     # if the config file is not in place
@@ -31,6 +69,11 @@ def get_config():
 
     with CORPPA_CONFIG_PATH.open() as cfg_file:
         try:
-            return yaml.load(cfg_file, Loader=Loader)
+            # configuration in the yaml file should override any defaults
+            config_overrides = yaml.load(cfg_file, Loader=Loader)
         except yaml.parser.ParserError as err:
             raise SystemExit(f"Error parsing config file: {err}")
+
+    # use default config as the starting point; settings loaded from the config
+    # file should override
+    return merge_dicts(DEFAULTS, config_overrides)
--- test/test_config.py
+++ test/test_config.py
@@ -5,6 +5,19 @@ import pytest
 from corppa import config
 
 
+def test_merge_dicts():
+    # no nesting, no conflict
+    assert config.merge_dicts({"a": 1}, {"b": 2}) == {"a": 1, "b": 2}
+    # no nesting; second overrides first
+    assert config.merge_dicts({"a": 1}, {"a": 2}) == {"a": 2}
+    # nesting, no conflict
+    assert config.merge_dicts({"a": {"b": 1}}, {"a": {"c": 2}}) == {
+        "a": {"b": 1, "c": 2}
+    }
+    # nesting, with override
+    assert config.merge_dicts({"a": {"b": 1}}, {"a": {"b": 2}}) == {"a": {"b": 2}}
+
+
 def test_get_config_not_found(tmp_path):
     test_config = tmp_path / "test.cfg"
     # error should include directions about how to fix the problem
@@ -33,11 +46,35 @@ def test_get_config(tmp_path):
     test_config = tmp_path / "test.cfg"
     test_config.write_text("""
 # local path to compiled poem dataset files
-poem_dataset:
-  data_dir: "/tmp/p-p-poems/data"
+compiled_dataset:
+  text_dir: "/tmp/p-p-poems/data"
 """)
     # use patch to override the config path and load our test file
     with patch.object(config, "CORPPA_CONFIG_PATH", new=test_config):
         config_opts = config.get_config()
-        assert "poem_dataset" in config_opts
-        assert config_opts["poem_dataset"]["data_dir"] == "/tmp/p-p-poems/data"
+        assert "compiled_dataset" in config_opts
+        assert config_opts["compiled_dataset"]["text_dir"] == "/tmp/p-p-poems/data"
+
+
+def test_get_config_defaults(tmp_path):
+    # create a test config file with one section and one value
+    test_config = tmp_path / "test.cfg"
+    # override one portion of a nested config
+    override_text_dir = "/ch/text.tar.gz"
+    test_config.write_text(f"""
+# local path to compiled poem dataset files
+reference_corpora:
+    chadwyck-healey:
+        text_dir: "{override_text_dir}"
+""")
+    # use patch to override the config path and load our test file
+    with patch.object(config, "CORPPA_CONFIG_PATH", new=test_config):
+        config_opts = config.get_config()
+        # override value should be used
+        chadwyck_healey_config = config_opts["reference_corpora"]["chadwyck-healey"]
+        assert chadwyck_healey_config["text_dir"] == override_text_dir
+        # default value in the same section that is not specified should be present
+        assert (
+            chadwyck_healey_config["metadata_path"]
+            == config.DEFAULTS["reference_corpora"]["chadwyck-healey"]["metadata_path"]
+        )
